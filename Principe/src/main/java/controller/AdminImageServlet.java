d2746e32e0d32bca0336d820f29ec9b05e2f5576
chanu2002 <chandrabanuherath07@gmail.com>
2026-03-07 02:00:02 +0530

    i

--- Principe/src/main/java/controller/AdminImageServlet.java
+++ Principe/src/main/java/controller/AdminImageServlet.java
@@ -1,33 +1,3 @@
-/*package controller;
-
-import java.io.*;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import dao.AdminDAO;
-
-public class AdminImageServlet extends HttpServlet {
-
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response)
-            throws ServletException, IOException {
-
-        String id = request.getParameter("id");
-
-        AdminDAO dao = new AdminDAO();
-        
-        byte[] image = dao.getImageById(id);
-
-        
-        
-        if (image != null) {
-            response.setContentType("image/jpeg");
-
-
-            response.getOutputStream().write(image);
-        }
-    }
-}*/
-
 package controller;
 
 import java.io.*;
